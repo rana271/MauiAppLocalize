7560e950bd37c9eb9b542aa76e9582fa7964dfd1
Rana <rana@gmail.com>
2024-02-27 09:28:30 +0530

    Add project files.

---
+++ MauiAppLocalize/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiAppLocalize.Resources.String;
+using System.Globalization;
+
+namespace MauiAppLocalize;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        AppResources.Culture = new CultureInfo("en");
+    }
+}
---
+++ MauiAppLocalize/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MauiAppLocalize.Resources.String;
+using System.Globalization;
+
+namespace MauiAppLocalize;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        
+    }
+}
+
